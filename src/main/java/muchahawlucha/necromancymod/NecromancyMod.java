825d98db6a8b0a64da1acbd46fad803e3c3abcbd
Eric Renart <eric.renart@stonybrook.edu>
2019-03-14 18:42:09 -0400

    add proxy sides

--- src/main/java/muchahawlucha/necromancymod/NecromancyMod.java
+++ src/main/java/muchahawlucha/necromancymod/NecromancyMod.java
@@ -1,8 +1,11 @@
 package muchahawlucha.necromancymod;
 
+import muchahawlucha.necromancymod.common.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
@@ -13,17 +16,30 @@ public class NecromancyMod
     public static final String NAME = "Zinric's Necromancy";
     public static final String VERSION = "0.1";
 
-    private static Logger logger;
+    // proxy
+    @SidedProxy(clientSide = "muchahawlucha.necromancymod.proxy.ClientProxy", serverSide = "muchahawlucha.necromancymod.proxy.serverProxy")
+    public static CommonProxy proxy;
+
+    // this mod's singleton instance
+    public static NecromancyMod nmInstance;
+    public static Logger logger;
+
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
+        proxy.preInit(event);
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
+        proxy.init(event);
+    }
 
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
     }
 }
---
+++ src/main/java/muchahawlucha/necromancymod/common/CommonProxy.java
@@ -0,0 +1,24 @@
+package muchahawlucha.necromancymod.common;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod.EventBusSubscriber
+public class CommonProxy {
+
+    // this is where most of the mod loading magic happens
+
+    public void preInit(FMLPreInitializationEvent e) {
+
+    }
+
+    public void init(FMLInitializationEvent e) {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+
+    }
+}
